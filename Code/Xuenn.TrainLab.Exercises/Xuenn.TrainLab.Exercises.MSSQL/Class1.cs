e5b38c022ff8f1ecee7d1831c9475b69eaa9a9b0
Chen-Tien Tsai <blackie1019@gmail.com>
2015-05-17 16:27:11 +0800

    Update Folder Structure

---
+++ Code/Xuenn.TrainLab.Exercises/Xuenn.TrainLab.Exercises.MSSQL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xuenn.TrainLab.Exercises.MSSQL
+{
+    public class Class1
+    {
+    }
+}
