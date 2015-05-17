1e0e7c8fc705c2e0261525e82d8c3171738e7405
Chen-Tien Tsai <blackie1019@gmail.com>
2015-05-17 15:33:16 +0800

    complete bower setting and MVC BundleConfig setting

--- Code/Xuenn.TrainLab.Site/App_Start/BundleConfig.cs
+++ Code/Xuenn.TrainLab.Site/App_Start/BundleConfig.cs
@@ -9,8 +9,26 @@ namespace Xuenn.TrainLab.Site
             BundleTable.Bundles.Clear();
             bundles.Clear();
 
+            #region Script
+
+            bundles.Add(new ScriptBundle("~/bundles/bower_compnents").Include(
+                "~/Content/bower_components/jquery/dist/jquery.min.js",
+                "~/Content/bower_components/bootstrap/dist/js/bootstrap.min.js",
+                "~/Content/bower_components/react/react.min.js"));
+
+            #endregion
+
+            #region Style
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "~/Content/bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
+                "~/Content/css/app.css"));
+
+            #endregion
+
 #if DEBUG
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = true;
 #else
             BundleTable.EnableOptimizations = true;
 #endif
