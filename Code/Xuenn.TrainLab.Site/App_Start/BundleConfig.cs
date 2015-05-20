b3edc7fc6a2d8e79b330962f25b534b01d298374
blackie.tsai <blackie.tsai@xuenn.com>
2015-05-20 15:50:29 +0800

    Update Bootstrap  theme

--- Code/Xuenn.TrainLab.Site/App_Start/BundleConfig.cs
+++ Code/Xuenn.TrainLab.Site/App_Start/BundleConfig.cs
@@ -15,6 +15,20 @@ namespace Xuenn.TrainLab.Site
 
             bundles.Add(new ScriptBundle("~/bundles/bower_compnents").Include(
                 "~/Content/bower_components/jquery/dist/jquery.min.js",
+                "~/Content/bower_components/bootstrap/dist/bootstrap.min.js",
+                "~/Content/bower_components/react/react.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/theme/js").Include(
+                "~/Content/theme/AdminLTE-master/plugins/fastclick/fastclick.min.js",
+               "~/Content/theme/AdminLTE-master/ dist/js/app.min.js",
+                "~/Content/theme/AdminLTE-master/plugins/sparkline/jquery.sparkline.min.js",
+                "~/Content/theme/AdminLTE-master/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+                "~/Content/theme/AdminLTE-master/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+                "~/Content/theme/AdminLTE-master/plugins/daterangepicker/daterangepicker.js",
+                "~/Content/theme/AdminLTE-master/plugins/datepicker/bootstrap-datepicker.js",
+                "~/Content/theme/AdminLTE-master/plugins/iCheck/icheck.min.js",
+                "~/Content/theme/AdminLTE-master/plugins/slimScroll/jquery.slimscroll.min.js",
+                "~/Content/theme/AdminLTE-master/plugins/chartjs/Chart.min.js",
                 "~/Content/bower_components/react/react.min.js"));
 
             #endregion
@@ -24,6 +38,11 @@ namespace Xuenn.TrainLab.Site
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/bower_components/bootstrap/dist/css/bootstrap.min.css",
                 "~/Content/bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
+                "~/Content/theme/AdminLTE-master/plugins/morris/morris.min.css",
+                "~/Content/theme/AdminLTE-master/plugins/jvectormap/jquery-jvectormap-1.2.2.min.css",
+                "~/Content/theme/AdminLTE-master/plugins/daterangepicker/daterangepicker-bs3.css",
+                "~/Content/theme/AdminLTE-master/dist/css/AdminLTE.min.css",
+                "~/Content/theme/AdminLTE-master/dist/css/skins/_all-skins.min.css",
                 "~/Content/css/app.css"));
 
             #endregion
